bbf80112f7a992b4992c8e236a0382973743f093
ManojithBhat <manojithbhat@gmail.com>
2024-12-05 14:51:32 +0530

    add db integration

---
+++ backend/src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+    res.send("API is running");
+})
+
+export {app}
